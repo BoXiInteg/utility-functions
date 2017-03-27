8bdd9d1c6e3515c365ea24d136128cd77a3bedf1
BoXiInteg <bxi@integconsulting.com>
2017-03-26 22:33:25 -0400

    update file

--- getInitials.js
+++ getInitials.js
@@ -1,6 +1,8 @@
 //"for your information" -> "FYI"
 function getInitials(name){
+   test;
    return name.split('   ')
      .map(word => word.charAt(0).toUpperCase())
      .join('');
+
 }
---
+++ getMonthName.js
@@ -0,0 +1,7 @@
+funtion getMonthName(place){
+   const months = [
+     'january',
+     'february',
+     'march',
+     'april'
+}
